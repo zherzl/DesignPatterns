ccc0fbde60eec52a312e85b911a4e1129c77123b
Zlatko <zlatko.herzl@outlook.com>
2018-10-21 11:47:16 +0200

    Rename

---
+++ CH05_StrategyPattern/ASPPatterns.Chap5.StrategyPattern.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPPatterns.Chap5.StrategyPattern.Model
+{
+    public class Class1
+    {
+    }
+}
