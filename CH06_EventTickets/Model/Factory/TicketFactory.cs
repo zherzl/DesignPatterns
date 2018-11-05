4dc40a14211c9e1509664fe5b5fcc87abff66cda
Zlatko <zlatko.herzl@outlook.com>
2018-11-05 22:40:10 +0100

    CH06 first commit

---
+++ CH06_EventTickets/Model/Factory/TicketFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Factory
+{
+    public class TicketPurchaseFactory
+    {
+        public static TicketPurchase CreateTicket(Event Event, int tktQty)
+        {
+            TicketPurchase ticket = new TicketPurchase();
+            ticket.Id = Guid.NewGuid();
+            ticket.Event = Event;
+            ticket.TicketQuantity = tktQty;
+            return ticket;
+        }
+    }
+
+    public class TicketReservationFactory
+    {
+        public static TicketReservation CreateReservation(Event Event, int tktQty)
+        {
+            TicketReservation reservation = new TicketReservation();
+            reservation.Id = Guid.NewGuid();
+            reservation.Event = Event;
+            reservation.ExpiryTime = DateTime.Now.AddMinutes(1);
+            reservation.TicketQuantity = tktQty;
+            return reservation;
+        }
+    }
+}
---
+++ CH06_EventTickets/Model/TicketPurchase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Model
+{
+    public class TicketPurchase
+    {
+        public Guid Id { get; set; }
+        public Event Event { get; set; }
+        public int TicketQuantity { get; set; }
+
+    }
+}
